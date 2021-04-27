2d71397b8511ad5a1c36fa814c90bb55808c6b7f
mywaiting <ourwaiting@gmail.com>
2021-04-27 17:48:45 +0800

    整理好几乎整个博客的实现，除了些许细节，几乎完整通过测试！

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,31 @@
  * (c) Kepatb. All rights reserved.
  */
 
+
+// search
+function initSiteSearch() {
+    var form = document.getElementById('search-form')
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()
+
+        var input = document.getElementById('search-input'),
+            action = form.action,
+            finalurl =  action + '?q=site:keptab.com%20' + input.value
+
+        if (!input) {
+            return
+        } else {
+            if (/ip[honead]|android|mobile|phone/i.exec(navigator.userAgent)) {
+                location.href = finalurl
+            } else {
+                window.open(finalurl, "_blank")
+            }
+        }
+        return false
+    })
+}
+
+// DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function() {
+    initSiteSearch()
+})
